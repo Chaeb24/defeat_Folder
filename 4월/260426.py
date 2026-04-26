78b40640f55b970b9475acd14f491c3ec853b5e2
Chaeb <chaebinpark05@gmail.com>
2026-04-26 12:43:32 +0900

    정렬 방식 어려웠다

---
+++ 4월/260426.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # 데이터 개수
+A = []
+
+for i in range(N):
+   A.append((int(input()), i)) # 인덱스 비교를 위해 튜플로 저장
+
+Max = 0
+sorted_A = sorted(A) # 리스트 정렬
+
+for i in range(N):
+    if Max < sorted_A[i][1] - i:
+        Max = sorted_A[i][1] -i
+
+print(Max+1) # swap 필요없는지 여부 체크 한번 더
---
+++ 4월/260426_02.py
@@ -0,0 +1,28 @@
+N = int(input()) # 데이터 개수
+A = list(map(int,input().split())) # 돈 인출하는데 필요한 시간
+S = [0] * N # 합 배열
+
+for i in range(1, N):
+    point = i
+    time = A[i] # 기준
+    for j in range(i-1,-1,-1):
+        if A[j] < A[i]:
+            point = j + 1
+            break
+        if j == 0:
+            point = 0
+    for j in range(i,point,-1):
+        A[j] = A[j-1]
+    A[point] = time
+
+S[0] = A[0]
+
+for i in range(1,N):
+    S[i] = S[i-1] + A[i] # 합 배열 만들기
+
+sum = 0
+
+for i in range(0,N):
+    sum += S[i]
+
+print(sum)
