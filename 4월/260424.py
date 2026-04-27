74c9781e5319ba6d5cbc89b268c565914dfd9531
Chaeb <jisusushi@gmail.com>
2026-04-27 13:13:33 +0900

    백준 17298 오류 수정

--- 4월/260424.py
+++ 4월/260424.py
@@ -9,7 +9,7 @@ ans = [0] * N # 결과값 저장
 for i in range(N):
     while stack and A[stack[-1]] < A[i]: # 스택의 맨 위
         ans[stack.pop()] = A[i] # 값이 크면 오큰수에 해당됨.
-        stack.append(i) # 인덱스 스택에 추가
+    stack.append(i) # 인덱스 스택에 추가
 
 while stack:
     ans[stack.pop()] = -1
