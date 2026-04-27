12eff125852cb14585c3d63580c300bd013a14aa
Chaeb <jisusushi@gmail.com>
2026-04-27 12:54:57 +0900

    런타임 에러 수정

--- 4월/study03_0417.py
+++ 4월/study03_0417.py
@@ -2,18 +2,19 @@
 import sys
 
 input = sys.stdin.readline
-suNo, quizNo = int(input().split())
-# suNo는 숫자개수, quizNo는 질의 개수
 
-numbers = list(map(int,input().split())) # 숫자 데이터 저장
+suNo, quizNo = map(int, input().split())
+
+numbers = list(map(int, input().split()))
+
 prefix_sum = [0]
 temp = 0
 
 for num in numbers:
-    temp = temp + num
+    temp += num
     prefix_sum.append(temp)
 
-for i in range(quizNo):
-    s,e = int(input().split())
-    ans = prefix_sum[e] - prefix_sum[s-1]
+for _ in range(quizNo):
+    s, e = map(int, input().split())
+    ans = prefix_sum[e] - prefix_sum[s - 1]
     print(ans)
