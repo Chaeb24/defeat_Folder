e1d4d0f9b984692823f3530656302282e3d77912
Chaeb <jisusushi@gmail.com>
2026-04-30 16:25:35 +0900

    백준 11724 DFS

---
+++ 4월/260430.py
@@ -0,0 +1,33 @@
+# 백준 11724
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n,m = int(input().split()) # 노드와 엣지 개수 입력
+visited = [False] * (n+1)
+A = [[] for _ in range(n+1)]
+
+def DFS(v):
+    visited[v] = True
+    for i in A[v]:
+        if not visited[i]:
+            DFS(i)
+
+for _ in range(m):
+    s,e = map(int,input().split())
+    A[s].append(e)
+    A[e].append(s)
+
+count = 0
+
+for i in range(1,n+1):
+    if not visited[i]:
+        count += 1
+        DFS(i)
+
+print(count)
+
+
+
+
+
